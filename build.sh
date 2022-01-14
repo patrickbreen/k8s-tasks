a8059623bb8c3073ca2851e73e38ce66b54ae9e8
Patrick <breen.patrick.1010@gmail.com>
2022-01-14 11:59:55 -0500

    ran gofmt and added it as pre-commit hook

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+
+# format source code:
+gofmt -w -s .
+
+# build docker image
 docker build -t registry.gitlab.com/leetcyber/leet-app:latest .
 
+# push docker image
 docker login registry.gitlab.com -u patrickbreen -p glpat-m-sAKKWW4huxAywb_J9A
-docker push registry.gitlab.com/leetcyber/leet-app
-
+docker push registry.gitlab.com/leetcyber/leet-app:latest
