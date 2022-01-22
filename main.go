8b828e4632b463b2ca1df8e1ee32d23441dcf0e8
Patrick <breen.patrick.1010@gmail.com>
2022-01-21 20:28:05 -0500

    add /metrics for prometheus

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/penglongli/gin-metrics/ginmetrics"
 )
 
 func main() {
@@ -14,6 +15,11 @@ func main() {
 	db.Init()
 
 	r := gin.Default()
+	m := ginmetrics.GetMonitor()
+	m.SetMetricPath("/metrics")
+	m.SetSlowTime(10)
+	m.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+	m.Use(r)
 
 	v1 := r.Group("/api/v1")
 	{
